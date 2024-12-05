7cb1ad8e80db61684e482e8bbd496c821f73cf6f
rianhasansiam <rianhasan1971@gmail.com>
2024-12-06 00:05:28 +0600

    loading spinner added

--- src/All Visas/AllVisas.jsx
+++ src/All Visas/AllVisas.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { contextData } from '../Contex'
 
@@ -7,15 +7,39 @@ import CardVisa from '../Home/CardVisa'
 const AllVisas = props => {
 
   const {allVisaData}=useContext(contextData)
+  const [showComponent, setShowComponent] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowComponent(true); 
+    }, 500);
+
+    // Clean up the timer if the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
   
   return (
-    <div className='container mx-auto grid md:grid-cols-2 max-sm:w-[90vw] lg:grid-cols-4 my-20 gap-5'>
+
+<>
+
+{showComponent ? (
+      <div className='container mx-auto grid md:grid-cols-2 max-sm:w-[90vw] lg:grid-cols-4 my-20 gap-5'>
 
       {
         allVisaData.map(eachVisaData => <CardVisa key={eachVisaData._id} visaData={eachVisaData} />)
       }
 
     </div>
+      ) : (
+        <div className="flex justify-center items-center my-[30vh]"><span className="loading loading-ring w-[100px] md:w-[150px]"></span></div>
+      )}
+
+</>
+   
+
+
+
+   
   )
 }
 
