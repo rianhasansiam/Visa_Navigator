85bd01cad0ba30f6d600a953d8803896fabd5149
rianhasansiam <rianhasan1971@gmail.com>
2024-12-06 16:55:07 +0600

    dark mode added

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-100 py-10">
+    <footer className="py-10">
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4 text-gray-700">
         <div>
           <h1 className="flex gap-3 items-center text-xl font-extrabold">
--- src/Home/TopCountries.jsx
+++ src/Home/TopCountries.jsx
@@ -6,7 +6,7 @@ const TopCountries = () => {
   const {topvisa}=useContext(contextData)
       
     return (
-      <div className="py-10 bg-white">
+      <div className="py-10 ">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-8">Top Countries for Visas</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
