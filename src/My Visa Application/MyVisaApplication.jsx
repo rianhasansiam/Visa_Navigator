365181b33dc2d083a6f9bc8f83759e708748edea
rianhasansiam <rianhasan1971@gmail.com>
2024-12-06 15:38:46 +0600

    serach option added

--- src/My Visa Application/MyVisaApplication.jsx
+++ src/My Visa Application/MyVisaApplication.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import SingleApplication from './SingleApplication';
 import { contextData } from '../Contex';
+import NoDataFound from './NoDataFound';
 
 const MyVisaApplication = props => {
  
@@ -19,22 +20,64 @@ const MyVisaApplication = props => {
 
 
 
+  //search data fetch
+  
+const [search,setSearch]=useState('')
+// console.log(search)
+
+useEffect(()=>{
+
+
+
+    fetch(`http://localhost:5000/my-visa-apply-by-search?searchPharams=${search}`)
+         .then(res => res.json())
+          .then(data => setAllVisaApply(data));
+  },[search,setAllVisaApply])
+
   
 
   return (
     <div className='my-10'>
 <h1 className="text-3xl font-bold mb-6 text-center">My Visa Applications</h1>
+
+
+{/* serarch option */}
+<label className="input input-bordered flex items-center gap-2 w-[20%] mx-auto my-10">
+  <input onChange={(e)=>setSearch(e.target.value)} type="text" className="grow" placeholder="Search" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    className="h-4 w-4 opacity-70">
+    <path
+      fillRule="evenodd"
+      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+      clipRule="evenodd" />
+  </svg>
+</label>
+
+
+
     
-    <div className='grid grid-cols-4 gap-5 container mx-auto  my-5'>
+      <div>
 
-    {
+    {allVisaApply.length===0?<NoDataFound/>:
 
-       allVisaApply.map(eachData => <SingleApplication key={eachData._id} singleDatavisa={eachData}  />)
-      
+     <div className='grid grid-cols-4 gap-5 container mx-auto  my-5'>
+
+       {
+
+           (allVisaApply.map(eachData => <SingleApplication key={eachData._id} singleDatavisa={eachData}  />))
+       }
+          </div>
     }
+      
+   
     
     </div>
     </div>
+    
+    
   )
 }
 
